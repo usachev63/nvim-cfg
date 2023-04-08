6182135491320e48eddef9a0ea60095c1827abb6
Danila Usachev <duesna897@gmail.com>
2023-04-08 17:58:09 +0300

    Removed branch from statusline

--- lua/lualine-cfg.lua
+++ lua/lualine-cfg.lua
@@ -1,5 +1,13 @@
 require('lualine').setup {
   options = {
     theme = 'gruvbox',
-  }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
 }
