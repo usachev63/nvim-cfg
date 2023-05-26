410d5b633ec0cc5957f39647e3250b5eb7380fba
Danila Usachev <duesna897@gmail.com>
2023-05-26 12:48:37 +0300

    Abbreviated :w to :Gw in git-repository buffers

--- lua/shar/common/fugitive.lua
+++ lua/shar/common/fugitive.lua
@@ -11,10 +11,26 @@ vim.api.nvim_set_keymap('n', '<Leader>dh', ':diffget //2<CR>', { noremap = true 
 vim.api.nvim_set_keymap('n', '<Leader>dl', ':diffget //3<CR>', { noremap = true })
 
 local augroup = vim.api.nvim_create_augroup('Fugitive', {})
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'fugitive',
   callback = function()
     vim.api.nvim_set_option_value('number', false, { scope = 'local', win = 0 })
+  end,
+  group = augroup,
+})
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
+  callback = function()
+    local git_dir = vim.fn.FugitiveGitDir()
+    if not git_dir or git_dir == '' then
+      return
+    end
+    vim.cmd {
+      cmd = 'cnoreabbrev',
+      args = { '<expr>', '<buffer>', 'w',
+        'getcmdtype() == ":" && getcmdline() == "w" ? "Gwrite" : "w"' }
+    }
   end,
   group = augroup,
 })
