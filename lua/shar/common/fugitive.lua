f7d29f5c05e5239709171aeddeb012e5154eeda4
Danila Usachev <duesna897@gmail.com>
2023-05-29 23:08:52 +0300

    Disabled :w to :Gwrite abbrevation for .gitignore d files

--- lua/shar/common/fugitive.lua
+++ lua/shar/common/fugitive.lua
@@ -12,6 +12,7 @@ vim.api.nvim_set_keymap('n', '<Leader>dl', ':diffget //3<CR>', { noremap = true 
 
 local augroup = vim.api.nvim_create_augroup('Fugitive', {})
 
+-- Disable line numbers in fugitive window
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'fugitive',
   callback = function()
@@ -20,16 +21,30 @@ vim.api.nvim_create_autocmd('FileType', {
   group = augroup,
 })
 
+-- Abbreviate :w to :Gwrite in tracked files
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
   callback = function()
+    if not vim.api.nvim_buf_is_valid(0) or vim.o.buftype ~= '' then
+      return
+    end
     local git_dir = vim.fn.FugitiveGitDir()
     if not git_dir or git_dir == '' then
       return
     end
+    local file_path = vim.api.nvim_buf_get_name(0)
+    local ls_files = vim.fn.FugitiveExecute {
+      "ls-files", "--", file_path
+    }
+    if #ls_files.stdout <= 1 then
+      return
+    end
     vim.cmd {
       cmd = 'cnoreabbrev',
-      args = { '<expr>', '<buffer>', 'w',
-        'getcmdtype() == ":" && getcmdline() == "w" ? "Gwrite" : "w"' }
+      args = {
+        '<expr>', '<buffer>',
+        'w',
+        'getcmdtype() == ":" && getcmdline() == "w" ? "Gwrite" : "w"'
+      },
     }
   end,
   group = augroup,
