2511ca247ef0fc561eda7c40b10a9a46da238eeb
Danila Usachev <usachev.danila@huawei.com>
2023-08-25 16:44:37 +0300

    Improved markdown installation

--- lua/shar/toolkit/markdown.lua
+++ lua/shar/toolkit/markdown.lua
@@ -4,9 +4,8 @@ function M.pack()
   local packer = require 'packer'
   packer.use {
     'iamcco/markdown-preview.nvim',
-    run = 'cd app && npm install',
-    setup = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
+    run = function()
+      vim.fn["mkdp#util#install"]()
     end,
     ft = { 'markdown' },
   }
