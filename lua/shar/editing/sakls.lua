237b119987964ab0a758e6cb51933221d4582239
Danila Usachev <duesna897@gmail.com>
2023-12-19 20:04:22 +0300

    Restored SAKLS

--- lua/shar/editing/sakls.lua
+++ lua/shar/editing/sakls.lua
@@ -5,12 +5,12 @@ local M = {}
 local options = require 'shar.options'
 
 function M.pack()
-  -- require('packer').use(options.editing.sakls.sakls_nvim_path)
+  require('packer').use(options.editing.sakls.sakls_nvim_path)
 end
 
 function M.set_up()
-  -- local sakls = require 'sakls'
-  -- sakls.init(options.editing.sakls.options)
+  local sakls = require 'sakls'
+  sakls.init(options.editing.sakls.options)
 end
 
 return M
