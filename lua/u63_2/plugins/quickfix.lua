3230312da470c48c00334303a68e35ebfd546749
Danila Usachev <duesna897@gmail.com>
2026-04-25 16:11:51 +0300

    lazy: persistent qf as a local plugin

--- lua/u63_2/plugins/quickfix.lua
+++ lua/u63_2/plugins/quickfix.lua
@@ -1,11 +1,21 @@
+local dirname = string.sub(debug.getinfo(1).source, 2, string.len('/quickfix.lua') * -1)
+
 return {
-  'kevinhwang91/nvim-bqf',
-  config = function()
-    local bqf = require 'bqf'
-    bqf.setup {
-      preview = {
-        auto_preview = false,
-      },
-    }
-  end,
+  {
+    'kevinhwang91/nvim-bqf',
+    config = function()
+      local bqf = require 'bqf'
+      bqf.setup {
+        preview = {
+          auto_preview = false,
+        },
+      }
+    end,
+  },
+  {
+    dir = dirname .. '/persistent-qf',
+    config = function()
+      require("persistent-qf").setup()
+    end,
+  },
 }
