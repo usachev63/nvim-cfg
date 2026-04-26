073e1de738e57b172ec0d1b2e43911582c98088b
Danila Usachev <duesna897@gmail.com>
2026-04-26 09:56:40 +0300

    lazy: custom on_attach + formatting

--- lua/u63_2/plugins/languages/cpp.lua
+++ lua/u63_2/plugins/languages/cpp.lua
@@ -7,9 +7,10 @@ vim.lsp.enable 'clangd'
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('u63/clangd_lsp_attach', {}),
   callback = function(args)
+    local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client.name == 'clangd' then
-      vim.keymap.set('n', '<LocalLeader>th', ':LspClangdSwitchSourceHeader<CR>')
+      vim.keymap.set('n', '<LocalLeader>th', ':LspClangdSwitchSourceHeader<CR>', { buffer = bufnr })
     end
   end,
 })
--- lua/u63_2/plugins/lsp.lua
+++ lua/u63_2/plugins/lsp.lua
@@ -1,3 +1,28 @@
+vim.diagnostic.config { signs = false }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('u63/lsp_attach', {}),
+  callback = function(args)
+    local bufnr = args.buf
+    local client_id = args.data.client_id
+
+    vim.keymap.set('n', '<Leader>fm', vim.lsp.buf.format, { buffer = bufnr })
+    local autofmt_augroup_name = 'u63/lsp_format/' .. bufnr
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = vim.api.nvim_create_augroup(autofmt_augroup_name, {}),
+      callback = function()
+        vim.lsp.buf.format {
+          async = false,
+          id = client_id,
+        }
+      end,
+    })
+    vim.keymap.set('n', 'yof', function()
+      vim.cmd('autocmd! ' .. autofmt_augroup_name)
+    end, { buffer = bufnr })
+  end,
+})
+
 return {
   'neovim/nvim-lspconfig',
 }
