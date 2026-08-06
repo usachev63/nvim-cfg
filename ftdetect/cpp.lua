19e5f9298f6ef8681c3ccca328155b67d1827797
Danila Usachev <duesna897@gmail.com>
2026-08-06 08:54:01 +0300

    Removed obsolete *.acm cpp ftdetect

--- ftdetect/cpp.lua
+++ ftdetect/cpp.lua
@@ -1,6 +1,6 @@
-local augroup = vim.api.nvim_create_augroup("CppExtraFtDetect", {})
+local augroup = vim.api.nvim_create_augroup("u63/cpp_extra_ftdetect", {})
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "*.tpp", "*.acm" },
+  pattern = { "*.tpp" },
   callback = function()
     vim.bo.filetype = 'cpp'
   end,
